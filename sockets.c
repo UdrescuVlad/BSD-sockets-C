38164032460247df6d49a40865627e9cece547fc
Vlad Udrescu <vlad.udrescu@unifiedpost.com>
2020-03-04 13:25:03 +0200

    added an example 2

--- sockets.c
+++ sockets.c
@@ -1,1 +1,22 @@
 #include<stdio.h>
+#include<stdlib.h>
+
+struct sockaddr {
+  unsigned short sa_family; //
+  char sa_data[14]; //unde vom stoca port si IP
+};
+
+struct sockaddr_in {
+  short sin_family;
+  unsigned short sin_port; // port number
+  struct in_addr sin_addr; // IP Address
+  char sin_zero[8];
+};
+
+struct in_addr {
+  unsigned long s_addr;
+};
+
+int main(){
+
+}
